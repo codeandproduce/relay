c50717c84f005f30a11091365eaad290cead7315
Student <student@PX-SL-04.local>
2019-04-19 17:19:41 +0800

    lap update

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ $(document).ready(function($){
 
 	var teamObj = {
 		beyondRunning: {
-			laps: 244,
+			laps: 314,
 			name: "Beyond Running",
 			id: "beyondRunning"
 		},				
@@ -77,7 +77,7 @@ $(document).ready(function($){
 			id: "benDing"
 		},								
 		cashMoney: {
-			laps: 176,
+			laps: 199,
 			name: "Cash Money On the Run",
 			id: "cashMoney"
 		},							
