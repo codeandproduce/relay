b57c75086d9d7e944e1a8bc10b96a5e2a9b8ecb3
Student <student@PX-SL-04.local>
2019-04-19 18:36:18 +0800

    lap update

--- js/main.js
+++ js/main.js
@@ -22,7 +22,7 @@ $(document).ready(function($){
 			id: "aggressivelyMediocre"
 		},			
 		motzko: {
-			laps: 157,
+			laps: 160,
 			name: "Mr. Motzko's Walk and Talk Exploratory",
 			id: "motzko"
 		},					
